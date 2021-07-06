076e3b720445a4cf29b6a43576d30198b94a84b3
Yuta iwasaki <yanyong0127@gmail.com>
2021-07-06 12:22:40 +0900

    Initial Commit

---
+++ NewsReaderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsReaderApp
+//
+//  Created by IwasakIYuta on 2021/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
